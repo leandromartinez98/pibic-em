9315d7bc1c609d7df246895836c138b69fa8b008
Leandro Martinez <leandro@iqm.unicamp.br>
2018-09-26 11:40:17 -0300

    added read file examples

---
+++ julia-exemplos/read_file.jl
@@ -0,0 +1,37 @@
+using PyPlot
+
+#
+# Reading a file using our custom read_data function (see Data.jl)
+#
+
+push!(LOAD_PATH,"./")
+using Data
+
+ndata, x, y = read_data("teste.dat", cols=[1,2], comment="#")
+
+plot(x,y)
+xlabel("x")
+ylabel("y")
+savefig("xy1.png")
+
+#
+# Reading a file using the already available readlm function from DelimitedFiles
+#
+
+using DelimitedFiles
+
+# this command reads all data ignoring commented lines
+data = readdlm("teste.dat",comments=true,comment_char='#')
+
+# get the number of lines (size of dimension 1 of the matrix)
+ndata = size(data,1)
+
+# set r and gmd as vectors containing the data in the first and second
+# columns of the data matrix
+x = [ data[i,1] for i in 1:ndata ]
+y = [ data[i,2] for i in 1:ndata ]
+
+plot(x,y)
+xlabel("x")
+ylabel("y")
+savefig("xy2.png")
