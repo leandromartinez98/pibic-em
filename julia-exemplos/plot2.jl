b64a917d506b5c9e3f4cb79dcd0ee9791ffc545e
Leandro Martinez <leandro@iqm.unicamp.br>
2018-10-25 14:57:21 -0300

    added plot2.jl

---
+++ julia-exemplos/plot2.jl
@@ -0,0 +1,26 @@
+
+# using Plots instead of PyPlot 
+
+using Plots
+pyplot()
+
+# Read data
+
+push!(LOAD_PATH,"./")
+using Data
+ndata, x, y = read_data("teste.dat", cols=[1,2], comment="#")
+
+plot(x,y,lw=2,color="red")
+
+annotate!([(5,20,text("look here",14,:red,:left))])
+
+plot!(size=(400,400),xlim=(0,10),ylim=(0,50))
+plot!(grid=false)
+
+plot!(xlabel="x",ylabel="y")
+plot!(title="Title")
+
+plot!(legend=false)
+
+savefig("plot2.pdf")
+
